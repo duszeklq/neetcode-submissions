4d5e7d12ddf71f874c3ff3d49f74425ef0d85999
duszeklq <duszekq133@gmail.com>
2026-06-09 13:59:50 +0200

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        seen = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement not in seen:
+                seen[nums[i]] = i 
+            else:
+                return [seen[complement], i]
+            
+# Optimal solution
+# Time Complexity: O(n)
+# Space Complexity: O(n)
